dc7de374e077189bc50a58ebe27f29b9a0342882
rcsang <sangmath@outlook.com>
2021-07-29 19:10:43 +0200

    added new pictures

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ const token = process.env.TOKEN;
 
 const prefix = '-';
 
+const harmonia = ['https://imgur.com/NT1hQEQ',
+'https://imgur.com/Djsj7QD',
+'https://imgur.com/alwksZN',
+'https://imgur.com/6B5TaSR',
+'https://imgur.com/tQ2L42x',
+'https://imgur.com/c3WxfFG',
+'https://imgur.com/XMJPlU0',
+'https://imgur.com/n8KhfzF',
+'https://imgur.com/guBppgi',
+'https://imgur.com/53jHl2R']
+
 client.once('ready', () => {
     console.log('Saladbot is online!')
 });
@@ -31,7 +42,7 @@ client.on('message', message => {
         message.channel.send('I am only a Baguette de pain complet :333');
     }
     else if(command === 'harmonia'){
-        message.channel.send('https://imgur.com/NT1hQEQ');
+        message.channel.send(harmonia[Math.floor(Math.random()*harmonia.length)]);
     }
 });
 
