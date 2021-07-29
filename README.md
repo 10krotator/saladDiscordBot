3faf8a05a10c514fcf08826c243fc5dd7a2b7714
rcsang <sangmath@outlook.com>
2021-07-29 18:22:21 +0200

    added more commands to bot

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ discord bot to spam ferret gifs
 
 ## Installation
 
-Execute `npm init`
-Install discord.js `npm install discord.js`
+- Execute `npm init`
+- Install discord.js `npm install discord.js`
+- Install dotenv package `npm i dotenv`
+- Create `.gitignore` file and exclude `.env` file that contains your TOKEN
+- Execute `main.js` using `node .` or `node main.js`
 
--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const client = new Discord.Client();
 require('dotenv').config();
 const token = process.env.TOKEN;
 
+const prefix = '-';
 
 client.once('ready', () => {
     console.log('Saladbot is online!')
-})
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+    else if(command === 'ferret'){
+        message.channel.send('https://media.giphy.com/media/uMP2hlGA7GYTu/giphy.gif');
+    }
+    else if(command === 'salad'){
+        message.channel.send('https://imgur.com/pf1tx1K');
+    }
+    else if(command === 'baguette'){
+        message.channel.send('https://media.giphy.com/media/fuEldTzs9rsJ0GDFoM/giphy.gif');
+    }
+    else if(command === 'whosthis'){
+        message.channel.send('I am only a Baguette de pain complet :333');
+    }
+});
+
 
 
 
