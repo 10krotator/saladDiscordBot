bd7a5596018be7b1e3885ed4ebbbe8c0a47472fe
rcsang <sangmath@outlook.com>
2021-08-01 16:20:27 +0200

    added random ferret gifs using giphy api

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ discord bot to spam ferret gifs
 - Install dotenv package `npm i dotenv`
 - Create `.gitignore` file and exclude `.env` file that contains your TOKEN
 - Execute `main.js` using `node .` or `node main.js`
-
+- create an app on heroku
+- Add heroku branch to repo `heroku git:remote -a appName`
+- Create proc file and add `worker node main.js`
+- Push code to heroku `git push heroku`
+- Change the scripts in package.json to `"start":"node main.js"`
--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
 require('dotenv').config();
 const token = process.env.TOKEN;
+const giphy_api_key = process.env.GIPHYAPIKEY;
+
 
 const prefix = '-';
 
+
+
+
+// pictures of harmonia from imgur
 const harmonia = ['https://imgur.com/NT1hQEQ',
 'https://imgur.com/Djsj7QD',
 'https://imgur.com/alwksZN',
@@ -14,13 +21,33 @@ const harmonia = ['https://imgur.com/NT1hQEQ',
 'https://imgur.com/XMJPlU0',
 'https://imgur.com/n8KhfzF',
 'https://imgur.com/guBppgi',
-'https://imgur.com/53jHl2R']
+'https://imgur.com/53jHl2R',
+'https://imgur.com/nUCWvFT',
+'https://imgur.com/FYcZmdM',
+'https://imgur.com/SdBcTK3',
+'https://imgur.com/ZXxo92l',
+'https://imgur.com/oDz8iVW'];
+
+
+
+
+
+// pictures of ferrets from giphy
+const giphy_url = "https://api.giphy.com/v1/gifs/search?api_key=";
+const giphy_endpoint = "&q=ferret&limit=50&offset=0&rating=g&lang=en";
+const ferret_api = giphy_url + giphy_api_key + giphy_endpoint;
+// API request for 25 ferret gifs
+
+// End of API request
+
 
 client.once('ready', () => {
-    console.log('Saladbot is online!')
+    console.log('Saladbot is online!');
 });
 
-client.on('message', message => {
+client.on('message', MessageFN);
+
+async function MessageFN(message){
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -30,7 +57,11 @@ client.on('message', message => {
         message.channel.send('pong!');
     }
     else if(command === 'ferret'){
-        message.channel.send('https://media.giphy.com/media/uMP2hlGA7GYTu/giphy.gif');
+        let response = await fetch(ferret_api);
+        let json = await response.json();
+        let index = Math.floor(Math.random()*json.data.length);
+        //console.log(json);
+        message.channel.send(json.data[index].url);
     }
     else if(command === 'salad'){
         message.channel.send('https://imgur.com/pf1tx1K');
@@ -42,9 +73,10 @@ client.on('message', message => {
         message.channel.send('I am only a Baguette de pain complet :333');
     }
     else if(command === 'harmonia'){
-        message.channel.send(harmonia[Math.floor(Math.random()*harmonia.length)]);
+        let index = Math.floor(Math.random()*harmonia.length);
+        message.channel.send(harmonia[index]);
     }
-});
+}
 
 
 
